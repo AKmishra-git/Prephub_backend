a61cefe823a2653187b6dceea8e8158168b1183a
Ayush Kumar Mishra <ak_mishra_mac@Mac.lan>
2026-02-21 22:48:06 +0530

    fix route paths for production

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
 const express = require('express');
-const videoRouter = require('../src/routes/video.routes')
-const authRouter = require("../src/routes/auth.routes")
-const cors = require("cors")
-const progressRouter = require("../src/routes/progress.routes")
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-const cookieparser = require("cookie-parser")
+const videoRouter = require('./routes/video.routes')
+const authRouter = require('./routes/auth.routes')
+const progressRouter = require('./routes/progress.routes')
+const app = express();
 
-
-const app = express()
-app.use(express.json())
-app.use(cookieparser())
+// middlewares
 app.use(cors({
   origin: "https://prephub-frontend.vercel.app",
   credentials: true
-}))
+}));
 
-app.use("/api/prep", authRouter)
-app.use("/api/prep", videoRouter)
-app.use("/api/prep/progress", progressRouter)
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
+app.use("/api/prep", authRouter);
+app.use("/api/prep", videoRouter);
+app.use("/api/prep/progress", progressRouter);
 
 module.exports = app;
