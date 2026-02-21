39b031ab1f4921a1591acb7b577435d9c688394c
Ayush Kumar Mishra <ak_mishra_mac@Mac.lan>
2026-02-21 22:35:13 +0530

    final fix cors production

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const connectToDB = require("./src/config/database")
 
 connectToDB()
 
-app.listen(5001, ()=>{
-    console.log("server started");
-    
+
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT, ()=>{
+    console.log("server started on port", PORT)
 })
