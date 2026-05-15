c730e52f3c5c1e814fa120518b402f915620a52b
Ayush Kumar Mishra <ak_mishra_mac@Mac.lan>
2026-05-15 17:35:08 +0530

    updated passport.js

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+
 const passport = require("passport")
 const GoogleStrategy = require("passport-google-oauth20").Strategy
 const userModel = require("../models/auth.model")
